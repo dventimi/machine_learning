b1031bc3dea7d36545df6b0f4f31583a79fa0768
David A. Ventimiglia <dventimi@gmail.com>
2017-10-11 10:52:59 -0700

    I think I have a solution for the 1st part

--- Week8/ex7/findClosestCentroids.m
+++ Week8/ex7/findClosestCentroids.m
@@ -4,12 +4,12 @@ function idx = findClosestCentroids(X, centroids)
 %   in idx for a dataset X where each row is a single example. idx = m x 1 
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
-
+  
 % Set K
-K = size(centroids, 1);
+  K = size(centroids, 1);
 
 % You need to return the following variables correctly.
-idx = zeros(size(X,1), 1);
+  idx = zeros(size(X,1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Go over every example, find its closest centroid, and store
@@ -20,14 +20,12 @@ idx = zeros(size(X,1), 1);
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
-
-
-
-
-
-
-
+  idx = cellfun(@(x)findClosestCentroid(x,centroids),
+		mat2cell(X,ones(1,size(X,1))));
 % =============================================================
-
 end
 
+function closest = findClosestCentroid(x, centroids)
+  d = x-centroids;
+  closest = nthargout(2,@sort,cellfun(@norm,mat2cell(d,ones(1,size(d,1)))))(1);
+end
